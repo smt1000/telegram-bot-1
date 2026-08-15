1d0df9fed379836ed001e3c3fba4d2e1d9898b55
smt1000 <smtgames1996@gmail.com>
2026-08-16 00:42:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,14 @@ def start(message):
             f"Could not load the image.\n\nError: {e}"
         )
         return
-
+	
+	
+    bot.send_message(
+        message.chat.id,
+       "Welcome to the questionnaire!\n\n"
+    "Read the situation below and choose the response "
+    "you think is best."
+    )
     # Send question and buttons
     bot.send_message(
         message.chat.id,
