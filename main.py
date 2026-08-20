c1716c4ed38cbb69eaae31a82a97953324031243
smt1000 <smtgames1996@gmail.com>
2026-08-20 06:32:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12719,7 +12719,7 @@ def question_70_answer(call):
             "The game is finished"
         )
 	#"""
-elif winner == "Charlotte":
+    elif winner == "Charlotte":
 
         bot.send_message(
             call.message.chat.id,
@@ -12754,7 +12754,7 @@ elif winner == "Charlotte":
         )
     IMAGE_URLEmilyEND = "https://i.ibb.co/BKtdGk4p/Emily-Final.jpg"
 
-    try:
+        try:
 
         response = requests.get(
             IMAGE_URLEmilyEND,
@@ -12780,7 +12780,7 @@ elif winner == "Charlotte":
             photo
         )
 
-    except Exception as e:
+        except Exception as e:
 
         print("QUESTION 70 IMAGE ERROR:", e)
 
